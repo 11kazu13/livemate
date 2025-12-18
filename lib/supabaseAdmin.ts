f1c6e915fd488e43f98c7377c96a400a78b58372
K <kazuki.ino01@gmail.com>
2025-12-18 13:44:38 +0900

    Update supabaseAdmin.ts

--- lib/supabaseAdmin.ts
+++ lib/supabaseAdmin.ts
@@ -1,9 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.sb_publishable_WoyuqBZiXrRKHj6BHvYHfQ_1Gxypm1a!;
-const serviceRoleKey = process.env.sb_secret_xF2zOElR7_OhvtUOlyoTUA_f3dnYLcy!;
+const supabaseUrl = process.env.SUPABASE_URL!;
+const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
 export const supabaseAdmin = createClient(supabaseUrl, serviceRoleKey, {
   auth: { persistSession: false },
 });
-
